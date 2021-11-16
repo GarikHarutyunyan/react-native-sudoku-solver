2a26cfa5451c20705e9e120e73c8c5c7b06649f5
Garik <garik.harutyunyan@zevit.net>
2021-11-17 01:49:29 +0400

    Hide StatusBar

--- App.tsx
+++ App.tsx
@@ -1,12 +1,15 @@
 import React from "react";
-import { StyleSheet, View } from "react-native";
+import { StatusBar, StyleSheet, View } from "react-native";
 import { Main } from "./src/pages/main/Main";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Main />
-    </View>
+    <>
+      <StatusBar hidden={true} />
+      <View style={styles.container}>
+        <Main />
+      </View>
+    </>
   );
 }
 
